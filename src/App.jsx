11bc0b4f5113f814a260f782dedb67644e135f3c
Dragbos <145892326+drakeRAGE@users.noreply.github.com>
2025-04-11 14:59:26 +0530

    Added configuration of vite + React + TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='h-screen text-5xl font-serif flex items-center justify-center text-blue-500 bg-pink-300'>Hello, I am Drag !!</h1>
+    </>
+  )
+}
+
+export default App
